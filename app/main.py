b323c062f0df9290bceebf402e2de4cd7a032c86
Ayush Rathore <kumaishere01@gmail.com>
2026-09-22 04:30:09 +0530

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-# Root ASGI Entrypoint for Uvicorn / Render
-from app.main import app
-
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
