8297c716c75658ca38ad4a5b5ec3892cd0359c4d
Ayush Rathore <kumaishere01@gmail.com>
2026-09-22 05:32:25 +0530

    Delete ShreeShyamToys directory

--- ShreeShyamToys/run_server.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import uvicorn
-import os
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    print(f"Starting Shree Shyam Toys Server on http://0.0.0.0:{port} ...")
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
